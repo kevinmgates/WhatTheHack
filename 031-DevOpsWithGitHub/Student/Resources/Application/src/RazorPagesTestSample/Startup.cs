e1a52fe653f726743165823444ac34f3c6b71fa1
Whowong <ahuang05@hotmail.com>
2021-06-18 16:30:35 -0700

    updating the app to support appinsights

--- 031-DevOpsWithGitHub/Student/Resources/Application/src/RazorPagesTestSample/Startup.cs
+++ 031-DevOpsWithGitHub/Student/Resources/Application/src/RazorPagesTestSample/Startup.cs
@@ -4,16 +4,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace RazorPagesTestSample
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
